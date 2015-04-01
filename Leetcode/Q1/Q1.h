8598eb43621407c8a6f044185ac63bc3bfd69215
Simon <youyouransjy@gmail.com>
2015-04-01 08:33:54 +0800

    initial the repo with a vs solution

---
+++ Leetcode/Q1/Q1.h
@@ -0,0 +1,45 @@
+#ifndef Q1_H
+#define Q1_H
+
+/*
+ Leetcode Q1
+ Given an array of integers, find two numbers such that they add up to a specific target number.
+
+ The function twoSum should return indices of the two numbers such that they add up to the target,
+ where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+ You may assume that each input would have exactly one solution.
+
+ Input: numbers={2, 7, 11, 15}, target=9
+ Output: index1=1, index2=2
+*/
+
+/******************************************************************/
+
+class Q1_1 
+{
+public:
+	vector<int> twoSum(vector<int> &numbers, int target) 
+	{
+
+	}
+}
+
+class Q1_2
+{
+public:
+	vector<int> twoSum(vector<int> &numbers, int target)
+	{
+
+	}
+}
+
+class Q1_3
+{
+public:
+	vector<int> twoSum(vector<int> &numbers, int target)
+	{
+
+	}
+}
+#endif//Q1_H
