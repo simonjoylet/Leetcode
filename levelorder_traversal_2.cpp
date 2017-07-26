cb19d4d9f93683037532fe6c4cd8af7e67f387c3
Simon <youyouransjy@gmail.com>
2017-07-26 16:29:55 +0800

    solve #107

--- levelorder_traversal_2.cpp
+++ levelorder_traversal_2.cpp
@@ -1,22 +1,22 @@
 /*
 ---------------
-Leetcode 102
+Leetcode 107
 ---------------
 
-Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
 
 For example:
 Given binary tree [3,9,20,null,null,15,7],
-    3
-   / \
-  9  20
-    /  \
-   15   7
+  3
+ / \
+9  20
+  /  \
+ 15   7
 return its level order traversal as:
 [
-[3],
+[15,7],
 [9,20],
-[15,7]
+[3]
 ]
 
 
@@ -46,7 +46,7 @@ public:
 		if (root) qu.push(root);
 		while (!qu.empty())
 		{
-			
+
 			queue<TreeNode *> levelQu;
 			vector<int> levelVec;
 			while (!qu.empty())
@@ -60,6 +60,8 @@ public:
 			rst.push_back(levelVec);
 			qu = levelQu;
 		}
+		// 相比层序遍历，reverse一下即可。
+		reverse(rst.begin(), rst.end());
 		return rst;
 	}
 };
